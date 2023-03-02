5bbca579300ece00618a5464af6b12215febe6b5
natalia-pavlenko <npavl56@gmail.com>
2023-03-02 23:23:47 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,12 @@
 import * as FetchImages from 'service/axios-api';
 import { useState, useEffect } from 'react';
 import Button from 'components/Button/Button';
-import Modal from 'components/Modal/Modal';
 import Searchbar from 'components/Searchbar/Searchbar';
 import ImageGallery from 'components/ImageGallery/ImageGallery';
-import { AppDiv, AppImg } from './App.styled';
 
 const App = () => {
   const [gallery, setGallery] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [largeImg, setLargeImg] = useState(null);
-  const [showModal, setShowModal] = useState(false);
   const [page, setPage] = useState(1);
   const [totalHits, setTotalHits] = useState(0);
 
@@ -33,37 +29,24 @@ const App = () => {
 
   const getSearchQuery = query => {
     if (query === '') {
-      alert ('Please enter text')
+      alert('Please enter text');
       return;
     }
     setSearchQuery(query);
     setPage(1);
     setGallery([]);
-   
-  };
-  const toggelModal = () => {
-    setShowModal(prev => !prev);
   };
 
-   const handleClickLoadMoreBtn = () => {
-      setPage(prev => prev + 1);
+  const handleClickLoadMoreBtn = () => {
+    setPage(prev => prev + 1);
   };
   const isShowBtn = gallery.length < totalHits;
-    return (
-      <>
-        <Searchbar onSubmit={getSearchQuery} />
-        {gallery && <ImageGallery gallery={gallery} />}
-        {showModal && (
-          <Modal onClose={toggelModal}>
-            <AppDiv>
-              <AppImg src={largeImg} alt="" />
-            </AppDiv>
-          </Modal>
-        )}
-
-        {isShowBtn && <Button onClick={handleClickLoadMoreBtn} />}
-      </>
-    );
-
+  return (
+    <>
+      <Searchbar onSubmit={getSearchQuery} />
+      {gallery && <ImageGallery gallery={gallery} />}
+      {isShowBtn && <Button onClick={handleClickLoadMoreBtn} />}
+    </>
+  );
 };
 export default App;
